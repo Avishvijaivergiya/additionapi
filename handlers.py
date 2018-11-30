798e1780b9deb817741f65dd3f8b6ba682f31086
Praneet Chandra <praneetsahaj@gmail.com>
2018-11-30 14:47:27 +0000

    Changes to code

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
-import wikipedia
+from os import environ
+
+
 def index(**kwargs):
     request = kwargs.get('request')
-    query = json.loads(request)
-    totalsum = sum(query.values())
-    return {"Sum of all numbers": totalsum}
+    totalsum = sum(request.values())
+    return {"summary": totalsum}

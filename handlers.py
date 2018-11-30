dabb40da497dfac858753f775b2c0834767698c2
Praneet Chandra <praneetsahaj@gmail.com>
2018-11-30 12:00:18 +0000

    additionnumbers

--- handlers.py
+++ handlers.py
@@ -1,16 +1,6 @@
 import wikipedia
-
-
 def index(**kwargs):
     request = kwargs.get('request')
-    if 'query' in request:
-        query = request.get('query')
-        try:
-            summary = wikipedia.summary(query, auto_suggest=False, redirect=False)
-        except Exception as e:
-            print('ERROR:', e)
-            summary = "No exact match found for {}".format(query)
-        finally:
-            return {"summary": summary}
-    else:
-        return {"error": "Must pass the 'query' parameter"}
+    query = json.loads(request)
+    totalsum = sum(query.values())
+    return {"Sum of all numbers": totalsum}
